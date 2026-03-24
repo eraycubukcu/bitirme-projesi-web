3f28fe13b2bc0ee0e6150323af5c031d8127d5c5
Eray Cubukcu <eraycubukcu691@gmail.com>
2026-03-24 23:56:32 +0300

    to do list eklendi.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,3 +10,15 @@ function App() {
 }
 
 export default App;
+
+/* To do list : 
+  - Validation kısmı nasıl yapılacak öğrenci no'ya göre mi ?
+  - admin panel 
+  - admin panelde öğrenci listeleme
+  - öğretmen atama sistemi
+  - hocaların max min alabileceği öğrenci sayısını ayarlama olayı
+  - admin panelde hoca ekle/sil/kontenjan ekle azalt
+  - ayrı bir kısım olarak tüm atamalara gerçekleştikten sonra tüm öğrenci - hoca listesini getir
+  - tüm öğrenciler atandıktan sonra excel dosyasına aktar butonu tarzı bir olay yapıp dosya şeklinde çıkartma eklenebilir!
+  - formun aktiflik durumunu ayarlama olayını düzenle
+*/
